c3dd113530fb4e41e24e2e9da218a85ac3d7a4c8
Jonathan Chen <sabza178@gmail.com>
2024-03-29 00:23:01 +0700

    add dto for account-service

---
+++ account/src/main/java/com/example/account/dto/AccountsDto.java
@@ -0,0 +1,13 @@
+package com.example.account.dto;
+
+import lombok.Data;
+
+@Data
+public class AccountsDto {
+
+    private Long accountNumber;
+
+    private String accountType;
+
+    private String branchAddress;
+}
---
+++ account/src/main/java/com/example/account/dto/CustomerDto.java
@@ -0,0 +1,13 @@
+package com.example.account.dto;
+
+import lombok.Data;
+
+@Data
+public class CustomerDto {
+
+    private String name;
+
+    private String email;
+
+    private String mobileNumber;
+}
