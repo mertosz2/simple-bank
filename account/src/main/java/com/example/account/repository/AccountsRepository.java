a9efeffbb2d91cc11dd1e8f8efab884ce224731f
Jonathan Chen <sabza178@gmail.com>
2024-04-03 21:44:33 +0700

    add CustomerException , findByMobllieNumber method

--- account/src/main/java/com/example/account/repository/AccountsRepository.java
+++ account/src/main/java/com/example/account/repository/AccountsRepository.java
@@ -1,10 +1,15 @@
 package com.example.account.repository;
 
 import com.example.account.entity.Accounts;
+import com.example.account.entity.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface AccountsRepository extends JpaRepository<Accounts, Long>{
+
+    Optional<Customer> findByMobileNumber(String mobileNumber);
 
 }
--- account/src/main/java/com/example/account/service/AccountsService.java
+++ account/src/main/java/com/example/account/service/AccountsService.java
@@ -1,9 +1,16 @@
 package com.example.account.service;
 
+import com.example.account.constants.AccountsConstants;
+import com.example.account.dto.CustomerDto;
+import com.example.account.entity.Accounts;
+import com.example.account.entity.Customer;
+import com.example.account.mapper.CustomerMapper;
 import com.example.account.repository.AccountsRepository;
 import com.example.account.repository.CustomerRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.Random;
 
 @RequiredArgsConstructor
 @Service
@@ -12,5 +19,21 @@ public class AccountsService {
     private final AccountsRepository accountsRepository;
     private final CustomerRepository customerRepository;
 
+    public void createAccount(CustomerDto customerDto){
+        Customer customer = CustomerMapper.mapToCustomer(customerDto, new Customer());
+        Customer savedCustomer = customerRepository.save(customer);
+        accountsRepository.save(createNewAccount(savedCustomer));
+    }
+
+    private Accounts createNewAccount(Customer customer){
+        Accounts newAccounts = new Accounts();
+        newAccounts.setCustomerId(customer.getCustomerId());
+        long randomAccNumber = 100000000L + new Random().nextInt(900000000);
+
+        newAccounts.setAccountNumber(randomAccNumber);
+        newAccounts.setAccountType(AccountsConstants.SAVINGS);
+        newAccounts.setBranchAddress(AccountsConstants.ADDRESS);
+        return newAccounts;
+    }
 
 }
